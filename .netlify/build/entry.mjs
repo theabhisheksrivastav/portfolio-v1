ed0fa80ffe740008c51d371ef01bf2534c656a64
Abhishek Srivastav <abhisheksrivastavofficial@gmail.com>
2025-08-17 18:43:20 +0530

    fix

---
+++ .netlify/build/entry.mjs
@@ -0,0 +1,38 @@
+import { renderers } from './renderers.mjs';
+import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
+import { manifest } from './manifest_BsQjvWbs.mjs';
+import { createExports } from '@astrojs/netlify/ssr-function.js';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/api/add-contact.astro.mjs');
+const _page2 = () => import('./pages/api/add-testimonial.astro.mjs');
+const _page3 = () => import('./pages/contact.astro.mjs');
+const _page4 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/api/add-contact.js", _page1],
+    ["src/pages/api/add-testimonial.js", _page2],
+    ["src/pages/contact.astro", _page3],
+    ["src/pages/index.astro", _page4]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    actions: () => import('./_noop-actions.mjs'),
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "70c0961d-2f96-4e90-920c-311e577f60d7"
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (Object.prototype.hasOwnProperty.call(serverEntrypointModule, _start)) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
